d54c55442f20cf89e0aa8aec4b9c0532da2ade3b
nawonjun12345 <164143890+nawonjun12345@users.noreply.github.com>
2024-04-11 03:01:47 +0900

    Add files via upload

---
+++ hw4.py.py
@@ -0,0 +1,11 @@
+def draw_line_string(name) :
+    msg1 = 'Hello ' + name+','
+    msg2 = 'welcome to Seoul.'
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+    print('-'*(nstr))
+    print(msg1)
+    print(msg2)
+    print('-'*(nstr))
+
+name =input('Input his/her name: ')
+draw_line_string(name)
