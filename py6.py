e809952822fcb8736f6e5418b935faa41f2a5363
nawonjun12345 <164143890+nawonjun12345@users.noreply.github.com>
2024-04-26 13:24:14 +0900

    Update py6.py

--- py6.py
+++ py6.py
@@ -1,12 +1,17 @@
-for i in range(2,6,1):
+for i in range(1,10,1):
     print("")
-    for g in range(1,10,1) :
-        a = str(i*g)
-        print(i,'x',g,'=',a.rjust(2), end='      ')
-
+    for g in range(2,6,1) :
+        a = i*g
+        print(g ,'x', i ,'=', f'{a:2d}', end='\t')
+        if g==5 :
+            print('')
 print("")  
-for i in range(2,6,1):
+
+for i in range(1,10,1):
     print("")
-    for g in range(1,10,1) :
-        a = str(i*g)
-        print(i,'x',g,'=',a.rjust(2), end='      ')
+    for g in range(6,10,1) :
+        a = i*g
+        print(g ,'x', i ,'=', f'{a:2d}', end='\t')
+        if g==9 :
+            print('')
+
