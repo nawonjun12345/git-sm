ba2e4fdc46e6ce86a575d8407087f78ff166e985
nawonjun12345 <164143890+nawonjun12345@users.noreply.github.com>
2024-04-26 12:37:54 +0900

    Add files via upload

---
+++ py6.py
@@ -0,0 +1,12 @@
+for i in range(2,6,1):
+    print("")
+    for g in range(1,10,1) :
+        a = str(i*g)
+        print(i,'x',g,'=',a.rjust(2), end='      ')
+
+print("")  
+for i in range(2,6,1):
+    print("")
+    for g in range(1,10,1) :
+        a = str(i*g)
+        print(i,'x',g,'=',a.rjust(2), end='      ')
